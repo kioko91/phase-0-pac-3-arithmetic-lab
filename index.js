549428910d7efcd0f227381c3290c40a98a97fe3
Calvin <calvinkioko91@gmail.com>
2022-06-05 02:14:47 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(n)
+{
+    n++;
+    return n;
+}
+function decrement(n)
+{
+    n--;
+    return n; 
+    
+}
+
+
+function makeInt(n){
+    parseInt(n,100);
+    return n;
+}
+function makeInt(n){
+    return parseInt(n,10);
+     
+}
+function preserveDecimal(n){
+    parseFloat(n,1);
+    return n;
+}
+function preserveDecimal(n){
+   return parseFloat(n);
+    
+}
+
